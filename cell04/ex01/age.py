bb557346cf20cfb3048522ed12a641ff8867ad9e
Pattiessse <117795758+Pattiessse@users.noreply.github.com>
2025-06-25 17:02:22 +0000

    cell04ex01

---
+++ cell04/ex01/age.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+age = input("Please tell me your age: ")
+n = int(age)
+print(f"You are currently {n} years old.")
+print(f"In 10 years, you'll be {n+10} years old.")
+print(f"In 20 years, you'll be {n+20} years old.")
+print(f"In 30 years, you'll be {n+30} years old.")
