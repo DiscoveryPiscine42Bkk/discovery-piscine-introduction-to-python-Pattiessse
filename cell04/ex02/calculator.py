b61048c9bf77555c2ebcb7012e03d9bf35af3ebe
Pattiessse <117795758+Pattiessse@users.noreply.github.com>
2025-06-25 17:07:50 +0000

    cell04ex02

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+a = int(input("give me the first number: "))
+b = int(input("give me the second number: "))
+print("Thank you!")
+c= a + b
+d = a - b
+f = a // b
+g = a * b
+print(a, "+", b, "=", c )
+print(a, "-", b, "=", d )
+print(a, "/", b, "=", f )
+print(a, "*", b, "=", g )
